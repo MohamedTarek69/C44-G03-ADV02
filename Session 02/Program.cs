4bbd686c1f1c1f21f7db24df62cb842c15dba0f8
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-08-27 21:05:23 +0300

    Q1

--- Session 02/Program.cs
+++ Session 02/Program.cs
@@ -4,7 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1)
+            //Given an array consists of numbers with size N and number of queries,
+            //in each query you will be given an integer X, and you should print how many numbers in array that is greater than  X
+            //Ex:
+            //Input
+            //3 3                    //Size of array , number of queries
+            //11 5 3             //Array 
+            //1                      //Query1
+            //5                     //Query2
+            //13                  //Query 3
+            //Output
+            //3                   //11,5,3
+            //1                  //11
+            //0
+
+            //Console.WriteLine("Enter size and number of queries: ");
+            //string[] Line = Console.ReadLine().Split();
+            //int Size, NumberOfQueries;
+            //int.TryParse(Line[0], out Size);
+            //int.TryParse(Line[1], out NumberOfQueries);
+
+            //Console.WriteLine("Enter your numbers: ");
+            //string[] Input = Console.ReadLine().Split();
+            //int[] arr = new int[Size];
+            //for (int i = 0; i < Size; i++)
+            //{
+            //    int.TryParse(Input[i], out arr[i]);
+            //}
+
+            //Console.WriteLine("Enter Queries: ");
+            //int[] queries = new int[NumberOfQueries];
+            //for (int i = 0; i < NumberOfQueries; i++)
+            //{
+            //    string input = Console.ReadLine();
+            //    int x;
+            //    int.TryParse(input, out x);
+            //    queries[i] = x;
+            //}
+
+            //Console.WriteLine("Output:");
+            //for (int i = 0; i < NumberOfQueries; i++)
+            //{
+            //    int count = 0;
+            //    for (int j = 0; j < Size; j++)
+            //    {
+            //        if (arr[j] > queries[i])
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //    Console.WriteLine(count);
+            //}
+
+            #endregion
         }
     }
 }
